28ae848fd4b18415c0a67361f5e854cd84b6c433
JeuFore <mat.chast67@hotmail.fr>
2020-05-01 16:45:47 +0200

    Change backend adress

--- front/src/utils/config.js
+++ front/src/utils/config.js
@@ -1,4 +1,4 @@
 export default {
-  localApiUrl: process.env.LOCAL_API_URL || 'http://glafore.ddns.net:1443/api',
+  localApiUrl: process.env.LOCAL_API_URL || 'https://glafore.ddns.net/api',
   demoMode: process.env.DEMO_MODE === 'true',
 };
