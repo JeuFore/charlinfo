d949181f64f6c894b66790c1db8f219fc79050f1
JeuFore <matis.chast67@hotmail.fr>
2020-05-02 16:18:12 +0200

    Add server WebSocket

--- server/controllers/fileClassController.js
+++ server/controllers/fileClassController.js
@@ -1,8 +1,8 @@
 const lib = require("./fileClass/lib.js");
 
-module.exports = function(app) {
+module.exports = function (app, websocketManager) {
   app.get("/file/:semester/:idclass/get", lib.getClass);
-  app.post("/file/:semester/:idclass/add", lib.addClass);
+  app.post("/file/:semester/:idclass/add", (req, res) => lib.addClass(req, res, websocketManager));
   app.post("/file/:semester/:idclass/delete", lib.deleteClass);
   app.get("/file/:semester/:idclass/download", lib.downloadClass);
   app.get("/file/:semester/:idclass/getTitle", lib.getClassTitle);
---
+++ server/webSocket.js
@@ -0,0 +1,122 @@
+const { request } = require('./controllers/requestController');
+
+const WebsocketManager = function WebsocketManager(wss, req) {
+    this.wss = wss;
+    this.connections = {};
+};
+
+/**
+ * @description Send a websocket message to the user.
+ * @param {Object} event - Event.
+ * @example
+ * sendMessageUser(event);
+ */
+
+async function verificationNotification(userId) {
+    const res = await request("select * from notification where iduser like $1 and show = 1", [userId]);
+    let message = {};
+    res.forEach(element => {
+        message.title = element.title;
+        message.description = element.description;
+        message.date = element.date;
+        this.connections[userId].client.send(JSON.stringify({ id: element.type, message }));
+    });
+    await request("update notification set show = 0 where iduser like $1", [userId]);
+}
+
+function sendMessageUser(userId, message) {
+    if (!this.connections[userId]) {
+        return 0;
+    }
+    this.connections[userId].client.send(JSON.stringify(message));
+}
+
+/**
+ * @description Send a websocket message to all user.
+ * @param {Object} event - Event.
+ * @example
+ * sendMessageAllUsers(event);
+ */
+async function sendMessageAllUsers(data) {
+    const user = await request("select id from compte");
+    let number = (await request("select id from notification order by id desc limit 1"))[0];
+    if (number)
+        number = number.id + 1;
+    else
+        number = 1;
+
+    const date = data.message.date;
+
+    user.forEach(element => {
+        request("insert into notification values($1, $2, $3, $4, $5, $6, $7)", [number, element.id, data.message.title, data.message.description, date, 1, data.type]);
+        number++;
+    });
+
+    const usersIds = Object.keys(this.connections);
+
+    usersIds.forEach((userId) => {
+        this.connections[userId].client.send(JSON.stringify(data));
+        request("update notification set show = 0 where iduser like $1", [userId]);
+    });
+}
+
+/**
+ * @description When a user is connected.
+ * @param {Object} user - User Object.
+ * @param {Object} client - Websocket client.
+ * @example
+ * userConnected(user, ws);
+ */
+function userConnected(user, client) {
+    if (!this.connections[user]) {
+        this.connections[user] = {};
+    }
+    this.connections[user] = { user, client };
+
+    this.verificationNotification(user);
+
+    return null;
+}
+
+/**
+ * @description When a user is disconnected.
+ * @param {Object} user - User Object.
+ * @param {Object} client - Websocket client.
+ * @example
+ * userDisconnected(user, ws);
+ */
+function userDisconnected(user) {
+    if (!this.connections[user]) {
+        this.connections[user] = [];
+    }
+    delete this.connections[user];
+    return null;
+}
+
+/**
+ * @description Init websocket server.
+ * @example
+ * init();
+ */
+function init() {
+    this.wss.on('connection', (ws) => {
+        ws.on('message', async (id) => {
+            try {
+                this.userConnected(id, ws);
+            } catch (e) {
+                console.log(e);
+                ws.close(4000);
+            }
+        });
+    });
+    return null;
+}
+
+WebsocketManager.prototype.init = init;
+WebsocketManager.prototype.userConnected = userConnected;
+WebsocketManager.prototype.userDisconnected = userDisconnected;
+WebsocketManager.prototype.sendMessageUser = sendMessageUser;
+WebsocketManager.prototype.sendMessageAllUsers = sendMessageAllUsers;
+WebsocketManager.prototype.verificationNotification = verificationNotification;
+
+module.exports = WebsocketManager;
